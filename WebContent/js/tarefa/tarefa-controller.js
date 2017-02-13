2e6e0a8a139b69560d0dad4a8a666eb8fc4d352f
jeronimoflima <jeronimoflima@yahoo.com.br>
2017-02-13 21:52:20 -0200

    OT 03 angular js

---
+++ WebContent/js/tarefa/tarefa-controller.js
@@ -0,0 +1,31 @@
+angular.module("tarefas").controller("TarefaController",function(){
+	this.tarefa = {};
+	
+	this.tarefas = [];
+	
+	this.novaTarefa = function(){
+		this.tarefa = {};
+	};
+	
+	this.salvarTarefa = function(tarefa){
+		tarefa.id = new Date().getTime();
+		this.tarefas.push(tarefa);
+		this.novaTarefa();
+	};
+	
+	this.removerTarefa = function(tarefa){
+		var pos = -1;
+		angular.forEach(this.tarefas,function(item,index){
+			if(tarefa.id == item.id){
+				pos = index;
+			}
+	});
+		if(pos > -1){
+			this.tarefas.splice(pos,1);
+			}
+		}
+		
+		this.selecionarTarefa = function(tarefa){
+			this.tarefa = tarefa;
+		}
+});
